655a6a1e2176f3e02de0b8a1c63348e99503671d
kbargach <bargachkhouloud@outlook.fr>
2022-05-03 06:41:16 -0700

    modifs

--- joueurs/joueur1.c
+++ joueurs/joueur1.c
@@ -9,10 +9,8 @@
 void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 	// Cette fonction peut appeler la fonction ecrireIndexCoup(coupChoisi);
 	// Pour sélectionner l'index d'un coup à jouer dans la liste l 
-	int i,j;
-	int voiscase,nbvois; 
-	octet o, d, v; 
-	int a; 
+	int i,j,voiscase,nbvois,nbv; 
+	octet o,d,vois; 
 	octet myColor = currentPosition.trait;
 	T_Voisins voisins;
 
@@ -33,7 +31,7 @@ void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 		}	
 	}
 
-// STRAT isolement adversaire
+// STRAT 3 isolement 1 voisin
 	for(i=0;i<listeCoups.nb; i++) {
 		o = listeCoups.coups[i].origine; 
 		d = listeCoups.coups[i].destination;
@@ -41,7 +39,37 @@ void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 		voisins = getVoisins(d);
 		nbvois=nbVoisins(d);
 
-		if(nbvois<=4 && currentPosition.cols[o].couleur==myColor && ){
+		nbv=0;
+		
+		for(j=0; j<nbvois;j++){
+			vois = voisins.cases[j];
+			if (currentPosition.cols[vois].couleur!=myColor){
+				nbv++;
+			}
+		}
+
+		if(nbv==0 && currentPosition.cols[o].couleur==myColor){
+			ecrireIndexCoup(i);
+			return;
+		}	
+	}
+
+// STRAT 4 isolement adversaire
+	for(i=0;i<listeCoups.nb; i++) {
+		o = listeCoups.coups[i].origine; 
+		d = listeCoups.coups[i].destination;
+
+		voisins = getVoisins(d);
+		nbvois=nbVoisins(d);
+		nbv=0;
+
+		for(j=0; j<nbvois;j++){
+			vois = voisins.cases[j];
+			if (currentPosition.cols[vois].couleur==myColor){
+				nbv++;
+			}
+		}
+		if(nbvois<=4 && currentPosition.cols[o].couleur==myColor && nbv>=3){
 			ecrireIndexCoup(i);
 			return;
 		}	
@@ -72,7 +100,7 @@ void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 // STRAT 2 prendre tour de 2 
 	for(i=0;i<listeCoups.nb; i++) {
 		o = listeCoups.coups[i].origine; 
-		d = listeCoups.coups[is].destination;
+		d = listeCoups.coups[i].destination;
 
 		if(currentPosition.cols[o].couleur==myColor && currentPosition.cols[o].nb+currentPosition.cols[d].nb==3){
 			ecrireIndexCoup(i);
